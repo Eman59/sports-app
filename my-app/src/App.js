31f388aace5a478044a433cd6d971c73596bf8a8
Eman59 <sayyedeman59@gmail.com>
2022-01-22 11:35:47 -0800

    api resolved

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,65 +4,53 @@ import "./app.css"
 
 function App() {
   const [players, setPlayers] = useState([]);
-
   const[search, setSearch] = useState('');
 
   useEffect(() => {
     async function getData() {
       const res = await axios.get(
-        `https://api.npoint.io/d6bd0efc05639084eb17/`
+        `https://run.mocky.io/v3/f55120ed-1fd6-4a38-943c-658c70edc966`
       );
-      console.log(res.data.playerList)
-      setPlayers(res.data.playerList);
-
+      console.log(res.data)
+      setPlayers(res.data);
     }
     getData();
   },[]);
 
   const filteredPlayers = players.filter( player => {
-    return player.PFName.toLowerCase().includes(search.toLowerCase())
+    return player.name.toLowerCase().includes(search.toLowerCase())
   })
 
   return (
     <div className="container">
-
       <div className="row">
         <div className="col-md-12">
-          <div class="form-group">
+          <div className="form-group">
             <input className="form-control" type="text" placeholder="Search Player...."  onChange={ e => setSearch(e.target.value)}/>
           </div>
         </div>
       </div>
-
       <div className="row">
-
         {
-          filteredPlayers.map((player, index) => {
+          filteredPlayers.map((player) => {
             return (
               <>
-
-                <div className="col-md-3">
-                  <div className="card mb-3" >
-                    <img src={`./player-images/${player.Id}.jpg`} className="card-img-top" alt="player.jpg"/>
+                <div className="col-md-3" key={player.id}>
+                  <div className="card mb-3">
+                    <img src={player.photos} className="card-img-top" alt="player.jpg"/>
                     <div className="card-body">
-                      <h5 className="card-title">{player.PFName}</h5>
-                      <p className="card-text"><span>Skill:</span> {player.SkillDesc}</p>
-                      <p className="card-text"> <span>Player Value: </span>${player.Value}</p>
-                      <p className="card-text"><span>Upcoming Matches: </span> {player.UpComingMatchesList[0].CCode} vs {player.UpComingMatchesList[0].VsCCode}</p>
-                      <p className="card-text"><span>MDate:</span> "{player.UpComingMatchesList[0].MDate}"</p>
+                      <h5 className="card-title">{player.name}</h5>
+                      <p><span>Age: </span>{player.age}</p>
+                      <p className="card-text"><span>Birth:</span> {player.birth}</p>
+                      <p className="card-text"> <span>Player Value: </span>€{player.value}</p>
                     </div>
                   </div>
                 </div>
-
               </>
             )
           })
         }
-
-
-
       </div>
-
     </div>
   );
 }
